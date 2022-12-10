671f7b25d674e184698dbd20ecff1efcc271a253
JuneJH <15310487388@163.com>
2022-12-10 17:34:21 +0800

    feat(*): 多环境配置

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,79 @@
-(()=>{var o={710:(o,e,l)=>{const n=l(653);console.log("module02 run in module01",n),console.log("module01 run overrr"),o.exports="module01"},653:o=>{console.log("module02 runnn"),o.exports="modlues02"}},e={};function l(n){var r=e[n];if(void 0!==r)return r.exports;var s=e[n]={exports:{}};return o[n](s,s.exports,l),s.exports}(()=>{console.log("玩转webpack");const o=l(710),e=l(653);console.log("module01=>",o),console.log("module02=>",e),console.log("index.js run over!!!")})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/modules/module01.js":
+/*!*********************************!*\
+  !*** ./src/modules/module01.js ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const module02 = __webpack_require__(/*! ./module02 */ "./src/modules/module02.js");
+
+console.log("module02 run in module01",module02);
+
+console.log("module01 run overrr");
+
+module.exports = "module01"
+
+
+/***/ }),
+
+/***/ "./src/modules/module02.js":
+/*!*********************************!*\
+  !*** ./src/modules/module02.js ***!
+  \*********************************/
+/***/ ((module) => {
+
+const str = "modlues02";
+console.log("module02 runnn")
+module.exports = str;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+console.log("玩转webpack");
+
+const module01 = __webpack_require__(/*! ./modules/module01 */ "./src/modules/module01.js");
+const module02 = __webpack_require__(/*! ./modules/module02 */ "./src/modules/module02.js");
+
+console.log("module01=>",module01);
+console.log("module02=>",module02);
+
+console.log("index.js run over!!!")
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
